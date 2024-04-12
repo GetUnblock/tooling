67c5306df7adc9f9f5d5b7e4e2d8bd2350e33c1b
helloiambguedes <bguedesdev@gmail.com>
2024-04-12 08:17:42 +0100

    fix: accommodated paginations on execution lists

--- misc/migrate-pages.js
+++ misc/migrate-pages.js
@@ -1,21 +1,27 @@
 "use strict"
 const fs = require('fs');
 
-const DIR = "artifacts/sm-execution-list/";
+const ARTIFACT_DIR = "artifacts/sm-execution-list/";
+const TEMP_DIR = "temp/executions/next-lists/";
 
-fs.readdir(DIR, (err, filenames) => {
+fs.readdir(TEMP_DIR, (err, filenames) => {
     if (err) {
         console.error(err);
         process.exit(1);
     }
     for (const file of filenames) {
-        const { executions, NextToken } = require("../" + DIR + file);
+        const { executions, NextToken } = require("../" + TEMP_DIR + file);
         const parsedExecutions = executions
             .map(execution => execution.executionArn)
             .join("\n");
         fs.writeFileSync("./temp/executions/" + file.replace(".json", ""), parsedExecutions);
+        const mainContent = require("../" + ARTIFACT_DIR + file);
+        mainContent.executions.push(...executions);
+        fs.writeFileSync(ARTIFACT_DIR + file, JSON.stringify(mainContent, null, 2));
         if (NextToken) {
             fs.writeFileSync("./temp/executions/next-tokens/" + file.replace(".json", ""), NextToken);
         }
     }
 });
+
+setTimeout(() => {}, 300);
--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ set -e
 
 mkdir -p temp
 mkdir -p temp/executions
+mkdir -p temp/executions/next-lists
+mkdir -p temp/executions/next-tokens
 mkdir -p artifacts
 mkdir -p artifacts/sm-execution-list
 mkdir -p artifacts/execution-history
@@ -12,17 +14,45 @@ aws stepfunctions list-state-machines > artifacts/state-machines.json
 
 echo "-> fetching exectution list of each state machine"
 node misc/transform-state-machines.js
+
 cat temp/sm-arns | while read smArn 
 do
-   aws stepfunctions list-executions --state-machine-arn=$smArn > artifacts/sm-execution-list/${smArn}.json
+   aws stepfunctions list-executions --max-items=100 --state-machine-arn=$smArn > artifacts/sm-execution-list/${smArn}.json
+done
+node misc/transform-execution-lists.js
+
+SHOULD_RECURSE=true
+
+FILES_IN_NEXT_TOKENS_DIR=$(ls -1q temp/executions/next-tokens/ | wc -l)
+if [ "$FILES_IN_NEXT_TOKENS_DIR" -eq "0" ]; then
+   SHOULD_RECURSE=false
+fi
+
+while $SHOULD_RECURSE
+do
+   for file in temp/executions/next-tokens/*
+   do
+      smArn="${file/temp\/executions\/next-tokens\//}"
+      if [[ "$smArn" != "*" ]]; then
+         aws stepfunctions list-executions --starting-token="$(cat $file)" --max-items=100 --state-machine-arn=$smArn > temp/executions/next-lists/${smArn}.json
+      fi
+   done
+   rm -rf temp/executions/next-tokens
+   mkdir -p temp/executions/next-tokens
+   node misc/migrate-pages.js
+   FILES_IN_NEXT_TOKENS_DIR=$(find temp/executions/next-tokens/ -maxdepth 1 -type f 2>/dev/null | wc -l)
+   if [ "$FILES_IN_NEXT_TOKENS_DIR" -eq "0" ]; then
+      SHOULD_RECURSE=false
+   fi
+   rm -rf temp/executions/next-lists
+   mkdir -p temp/executions/next-lists
 done
 
 echo "-> fetching executing history"
-node misc/transform-execution-lists.js
 for file in temp/executions/*
 do
    cat $file | while read execution 
    do
-      aws stepfunctions get-execution-history --execution-arn=$execution > artifacts/execution-history/${execution}.json
+      aws stepfunctions get-execution-history --execution-arn=$execution > artifacts/execution-history/${execution}.json || continue
    done
 done
